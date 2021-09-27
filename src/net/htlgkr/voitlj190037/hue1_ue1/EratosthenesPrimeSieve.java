f4ccf20316b4f9294add14e3b354113167d87373
Jonas Voitl <VoitlJ190037@sus.htl-grieskirchen.at>
2021-09-27 12:26:26 +0200

    first version (SÜ)

--- src/net/htlgkr/voitlj190037/hue1_ue1/EratosthenesPrimeSieve.java
+++ src/net/htlgkr/voitlj190037/hue1_ue1/EratosthenesPrimeSieve.java
@@ -1,5 +1,49 @@
 package net.htlgkr.voitlj190037.hue1_ue1;
 
-public class EratosthenesPrimeSieve {
+import java.util.HashMap;
+import java.util.Map;
 
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    private final int MAX;
+    private int numbers[];
+    private boolean isPrime[];
+
+    public EratosthenesPrimeSieve(int max) {
+        this.MAX = max;
+    }
+
+    public void getPrimes() {
+        numbers = new int[MAX];
+        isPrime = new boolean[MAX];
+
+        //Array wird mit Nummern befüllt
+        for(int i=0; i<numbers.length; i++) {
+            numbers[i]=i;
+        }
+        for(int i=2; i<isPrime.length; i++) {
+            if(isPrime[i]==true) {
+                for(int j=i; j<numbers.length; j++) {
+                    if(numbers[j]%i==0) {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+        }
+        printPrimes();
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+        for(int i=0; i<numbers.length; i++) {
+            if(isPrime[i]==true) {
+                System.out.println(numbers[i]+"\n");
+            }
+        }
+    }
 }
---
+++ src/net/htlgkr/voitlj190037/hue1_ue1/PrimeSieve.java
@@ -0,0 +1,7 @@
+package net.htlgkr.voitlj190037.hue1_ue1;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+
+    public void printPrimes();
+}
