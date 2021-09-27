7fa05bfbb46ab1a0fc3ed85c9385e85604affcaa
Jonas Voitl <VoitlJ190037@sus.htl-grieskirchen.at>
2021-09-27 22:39:34 +0200

    second version (HÜ)

--- src/net/htlgkr/voitlj190037/hue1_ue1/EratosthenesPrimeSieve.java
+++ src/net/htlgkr/voitlj190037/hue1_ue1/EratosthenesPrimeSieve.java
@@ -1,32 +1,21 @@
 package net.htlgkr.voitlj190037.hue1_ue1;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
-    private final int MAX;
-    private int numbers[];
-    private boolean isPrime[];
+    private boolean[] isPrime;
 
     public EratosthenesPrimeSieve(int max) {
-        this.MAX = max;
-    }
+        isPrime = new boolean[max];
+        Arrays.fill(isPrime, true);
 
-    public void getPrimes() {
-        numbers = new int[MAX];
-        isPrime = new boolean[MAX];
-
-        //Array wird mit Nummern befüllt
-        for(int i=0; i<numbers.length; i++) {
-            numbers[i]=i;
-        }
-        for(int i=2; i<isPrime.length; i++) {
+        for(int i=0; i<isPrime.length; i++) {
             if(isPrime[i]==true) {
-                for(int j=i; j<numbers.length; j++) {
-                    if(numbers[j]%i==0) {
-                        isPrime[j] = false;
-                    }
+                for(int j=i; j<isPrime.length; j=j+i) {
+                    isPrime[j]=false;
                 }
             }
         }
@@ -40,9 +29,9 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public void printPrimes() {
-        for(int i=0; i<numbers.length; i++) {
+        for(int i=0; i<isPrime.length; i++) {
             if(isPrime[i]==true) {
-                System.out.println(numbers[i]+"\n");
+                System.out.println(i+"\n");
             }
         }
     }
