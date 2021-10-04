b8c183262ab5bc037c7d555ee9f17fd548f708e0
Jonas Voitl <VoitlJ190037@sus.htl-grieskirchen.at>
2021-10-04 12:32:45 +0200

    final commit (SÜ)

--- src/net/htlgkr/voitlj190037/hue1_ue1/Main.java
+++ src/net/htlgkr/voitlj190037/hue1_ue1/Main.java
@@ -1,8 +1,26 @@
 package net.htlgkr.voitlj190037.hue1_ue1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(200);
+        Scanner s = new Scanner(System.in);
+        int limit = 0;
+        do {
+            System.out.println("Geben Sie eine Obergrenze ein. Diese muss Element der natürlichen Zahlen sein.");
+            try {
+                limit = Integer.parseInt(s.nextLine());
+                if(limit<4) {
+                    throw new IllegalArgumentException();
+                }else {
+                    break;
+                }
+            }catch (IllegalArgumentException e) {
+                System.out.println("Dies ist ein ungültiger Wert.");
+            }
+        }while(true);
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(limit);
         eps.calcPrimes();
+        eps.printPrimes();
     }
 }
